816e4b863b5b3908b3689cb413d75a059f2a06b3
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-09-22 22:53:18 -0300

    Node vs MySQL

--- node/index.js
+++ node/index.js
@@ -1,11 +1,27 @@
 const express = require('express')
+const mysql = require('mysql')
+
 const app = express()
 const PORT = 3000
+
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb',
+}
+
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO PEOPLE(name) values('Wesley')`
+
+connection.query(sql)
+connection.end()
 
 app.get('/', (_, res) => {
   res.send('<h1>Full Cycle</h1>')
 })
 
 app.listen(PORT, () => {
-  console.log(`Running on port ${PORT}`)
+  console.log(`Rodando na porta ${PORT}`)
 })
